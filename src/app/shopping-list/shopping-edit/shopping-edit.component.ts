564371e05054c1c9e138442d0148837bc2d6f479
JanGospodarek <jan.gospo@onet.pl>
2023-04-10 11:15:22 +0200

    nav

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient-model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
-
+  @ViewChild('nameInput', { static: true }) nameInpRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInpRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInpRef.nativeElement.value,
+      this.amountInpRef.nativeElement.value
+    );
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
